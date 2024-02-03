90990bfbf9e42e6198d85855e3330bf39878ee86
DavldMA <imaginaqueumghoul@gmail.com>
2024-02-03 19:40:45 +0000

    ganda lista feita, agora so o alex colocar nos detalhes

--- app/src/main/java/com/example/shopping_list_application/placeholder/ListDetailsActivity.java
+++ app/src/main/java/com/example/shopping_list_application/placeholder/ListDetailsActivity.java
@@ -1,30 +1,64 @@
 package com.example.shopping_list_application.placeholder;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.shopping_list_application.R;
 
 public class ListDetailsActivity extends AppCompatActivity {
-
-
     ListView listViewData;
     ArrayAdapter<String> adapter;
     String[] arrayXD = {"Conteudo1", "conteudo2", "produto3"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_details);
         listViewData = findViewById(R.id.lv_data);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, arrayXD);
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item_layout, R.id.checkTextView, arrayXD) {
+            @NonNull
+            @Override
+            public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+
+                Button buttonDelete = view.findViewById(R.id.buttonDelete);
+
+                buttonDelete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //HANDLE DELETE HERE ALEX
+                    }
+                });
+
+                return view;
+            }
+        };
         listViewData.setAdapter(adapter);
+
+        listViewData.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        listViewData.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                CheckedTextView checkedTextView = (CheckedTextView) view.findViewById(R.id.checkTextView);
+                checkedTextView.setChecked(!checkedTextView.isChecked());
+                Log.i("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "" + checkedTextView.isChecked());
+            }
+        });
+
     }
 
     @Override
